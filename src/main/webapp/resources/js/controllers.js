37454f8c2260afe91fc2d42d21fb5ffb73b1739e
runapal5 <runapal5@gmail.com>
2017-01-15 19:43:41 +0530

    Add files via upload

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -4,13 +4,13 @@
 
 
 function ProductController($scope, $http)  {
-  $http.get('http://172.17.0.5:8080/productlist/listProducts').success(function(data) {
+  $http.get('http://172.17.0.7:80/productlist/listProducts').success(function(data) {
             $scope.products = data;
         });  
 }
 
 function ProductDetailController($scope, $routeParams, $http){
-    $http.get('http://172.17.0.5:8080/productlist/product?productID=' + $routeParams.productID).success(function(data) {
+    $http.get('http://172.17.0.7:80/productlist/product?productID=' + $routeParams.productID).success(function(data) {
       $scope.product = data;
     });
 }
@@ -19,11 +19,11 @@ function UserProfileController($scope, $routeParams, $http, $rootScope){
     
     $rootScope.username = 'aziz';
     
-    $http.get('http://172.17.0.5:8080/productlist/addProductToBasket?username='+ $rootScope.username +'&productID=' + $routeParams.productID).success(function(data) {
+    $http.get('http://172.17.0.7:80/productlist/addProductToBasket?username='+ $rootScope.username +'&productID=' + $routeParams.productID).success(function(data) {
       $scope.products = data;
     });
     
-    $http.get('http://172.17.0.5:8080/productlist/userDetails?username=' + $rootScope.username).success(function(data) {
+    $http.get('http://172.17.0.7:80/productlist/userDetails?username=' + $rootScope.username).success(function(data) {
       $scope.user = data;
     });
     
